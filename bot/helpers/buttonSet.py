358aa8913fcdadc45eaa7267ce027188466eb97d
Udith <uaudith@gmail.com>
2021-12-12 19:16:25 +0530

    fix verify button

--- bot/helpers/buttonSet.py
+++ bot/helpers/buttonSet.py
@@ -1,11 +1,12 @@
-from pyrogram.raw.types import InputUserSelf
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, LoginUrl, InlineKeyboardButton
 
 from bot.config import config
-from ..myInlineKeyboardBtn import myInlineKeyboardButton
 
 
 def authUrlButtons(chatID: int):
-    btn = myInlineKeyboardButton(text="Verify", url=config.FQDN+f"/{chatID}",
-                                 fwd_text="why forward the message", bot=InputUserSelf())
+    loginUrl = LoginUrl(
+        url=config.FQDN + f"/{chatID}",
+        forward_text="why forward the message",
+        bot_username=config.BOT_USERNAME)
+    btn = InlineKeyboardButton(text="Verify", login_url=loginUrl)
     return InlineKeyboardMarkup([[btn]])
